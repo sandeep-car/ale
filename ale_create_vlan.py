e1529b929685fd38c9df02b6a3fdef0389dcff44
Sandeep Cariapa <cariapa@gmail.com>
2018-12-05 17:11:18 -0800

    Add files via upload

---
+++ ale_create_vlan.py
@@ -0,0 +1,110 @@
+#!/usr/local/bin/python3.7
+#
+# DISCLAIMER: This script is not supported by Nutanix. Please contact
+# Sandeep Cariapa (firstname.lastname@nutanix.com) if you have any questions.
+# NOTE:
+# Please read the HOWTO and README files included with this distribution before proceeding.
+# For reference look at:
+# https://github.com/sandeep-car/api-lab
+# https://github.com/nutanix/Connection-viaREST/blob/master/nutanix-rest-api-v2-script.py
+import sys
+import json
+import uuid
+import requests
+import argparse
+import clusterconfig as C
+from pprint import pprint
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+# Sample_vm2 has an attached ISO of Fedora Core.
+student_pod = C.ALE_VMS
+
+# Network name prefix.
+network_name_prefix = "ale"
+
+if __name__ == "__main__":
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("num_students", type=int, help="Number of student pods.")
+        args = parser.parse_args()
+        
+        num_students = args.num_students
+        
+        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+        mycluster = C.my_api(C.cluster_ip, C.cluster_admin, C.cluster_pwd)
+        status, cluster = mycluster.get_cluster_information()
+        if status != 200:
+            print("Cannot connect to: %s" % cluster)
+            print(">>> Did you remember to update the config file? <<<")
+            sys.exit(1)
+        
+        # Get specific cluster elements.
+        print("Name: %s." % cluster["name"])
+        print("ID: %s." % cluster["id"])
+        print("Cluster Ext IP Address: %s." % cluster["cluster_external_ipaddress"])
+        print("Number of Nodes: %s." % cluster["num_nodes"])
+        print("Version: %s." % cluster["version"])
+
+        # Get VM info so we can grab UUIDs of VMs we need to clone.
+        status, all_vms = mycluster.get_all_vm_info()
+        all_vms_list = all_vms["entities"]
+        # pprint(all_vms_list)
+
+        # Get list of available VLANs.
+        vlan_list = mycluster.get_usable_vlans(num_students)
+        print("Usable vlans:")
+        pprint(vlan_list)
+        if len(vlan_list) < num_students:
+            print ("Sorry dude, you've run out of VLANs. Free some up, or choose a fewer number of students.")
+            sys.exit(1)
+
+        networks_byvlan = {}
+        # Create network/VLANs, one for each student.
+        for vlan in vlan_list:
+            print ("Creating Vlan: ", vlan)
+
+            status, resp = mycluster.create_network(vlan,network_name_prefix)
+            print("Status, Resp", status, resp)
+            if status != 201:
+                print("Could not create VLAN:",vlan,status)
+                pprint(resp)
+                sys.exit(1)
+            
+            networks_byvlan[vlan] = resp["network_uuid"]
+
+        # Create student PODs.
+        # Outer loop is for VLANs, because each POD must be within its own VLAN.
+        for vlan,network_uuid in networks_byvlan.items():
+            for clone_source in student_pod:
+                print ("Clone_Source: ", clone_source)
+                
+                for vm_dict in all_vms_list:
+                    if vm_dict["name"] != clone_source:
+                        continue
+                    # If vm_dict["name"] matches the clone_source.
+                    vm_name = clone_source + "." + str(vlan)
+                    vm_uuid = vm_dict["uuid"]
+                    status, cloneuuid, resp = mycluster.clonevm(vm_uuid,vm_name,network_uuid)
+                    print("Status, CloneUUID, Resp", status, cloneuuid, resp)
+                    if status != 201:
+                        print ("Could not clone:",vm_name,status)
+                        pprint(resp)
+                        sys.exit(1)
+                    
+                    # Poll the task UUID until complete.
+                    taskid = resp["task_uuid"]
+                    mycluster.poll_task(taskid)
+                    
+                    # Power on the cloned VM.
+                    status,resp = mycluster.power_on_vm(cloneuuid)
+                # End for vm_dict.
+            # End for clone_source.
+        # End for vlan.
+                        
+        
+        print("=")
+        print("*COMPLETE*")
+
+    except Exception as ex:
+        print(ex)
+        sys.exit(1)
---
+++ clusterconfig.py
@@ -0,0 +1,213 @@
+# DISCLAIMER: This script is not supported by Nutanix. Please contact
+# Sandeep Cariapa (firstname.lastname@nutanix.com) if you have any questions.
+import json
+import uuid
+import requests
+from pprint import pprint
+from urllib.parse import quote
+
+# Cluster credentials.
+cluster_ip = "10.21.101.37"
+cluster_admin = "restapiuser"
+cluster_pwd = "blah blah"
+
+# Golden VMs. These are the VMs that are members of each student POD.
+# These could be read in from a CSV file provided as an argument also.
+ALE_VMS = [ "sample_vm1", "sample_vm2"]
+
+
+# ========== DO NOT CHANGE ANYTHING UNDER THIS LINE =====
+class my_api():
+    def __init__(self,ip,username,password):
+        
+        # Cluster IP, username, password.
+        self.ip_addr = ip
+        self.username = username
+        self.password = password
+        # Base URL at which v0.8 REST services are hosted in Prism Gateway.
+        base_urlv08 = 'https://%s:9440/PrismGateway/services/rest/v0.8/'
+        self.base_urlv08 = base_urlv08 % self.ip_addr
+        self.sessionv08 = self.get_server_session(self.username, self.password)
+        # Base URL at which v1 REST services are hosted in Prism Gateway.
+        base_urlv1 = 'https://%s:9440/PrismGateway/services/rest/v1/'
+        self.base_urlv1 = base_urlv1 % self.ip_addr
+        self.sessionv1 = self.get_server_session(self.username, self.password)
+        # Base URL at which v2 REST services are hosted in Prism Gateway.
+        base_urlv2 = 'https://%s:9440/PrismGateway/services/rest/v2.0/'
+        self.base_urlv2 = base_urlv2 % self.ip_addr
+        self.sessionv2 = self.get_server_session(self.username, self.password)
+        # Base URL at which v2 REST services are hosted in Prism Gateway.
+        base_urlv3 = 'https://%s:9440/PrismGateway/services/rest/v3.0/'
+        self.base_urlv3 = base_urlv3 % self.ip_addr
+        self.sessionv3 = self.get_server_session(self.username, self.password)
+        
+    def get_server_session(self, username, password):
+
+        # Creating REST client session for server connection, after globally
+        # setting authorization, content type, and character set.
+        session = requests.Session()
+        session.auth = (username, password)
+        session.verify = False
+        session.headers.update({'Content-Type': 'application/json; charset=utf-8'})
+        return session
+
+    # Get cluster information.
+    def get_cluster_information(self):
+
+        cluster_url = self.base_urlv2 + "cluster/"
+        print("Getting cluster information for cluster", self.ip_addr)
+        try:
+            server_response = self.sessionv2.get(cluster_url)
+            return server_response.status_code ,json.loads(server_response.text)
+        except Exception as ex:
+            print(ex)
+            return -1,cluster_url
+
+    # Get list of all VMs.
+    def get_all_vm_info(self):
+
+        cluster_url = self.base_urlv2 + \
+                      "vms/?include_vm_disk_config=true&include_vm_nic_config=true"
+        server_response = self.sessionv2.get(cluster_url)
+        # print("Response code: ",server_response.status_code)
+        return server_response.status_code, json.loads(server_response.text)
+
+    # Clone a VM with the given UUID
+    def clonevm(self,vmid,vm_name,network_uuid):
+
+        # If the cloned VM was to have > 1 NIC, you would send over a list of network UUIDs.
+        # And loop through that here to fill out vm_nics[]
+        vm_nic={}
+        vm_nic["network_uuid"] = network_uuid
+        vm_nics=[]
+        vm_nics.append(vm_nic)
+
+        print("VM_NICS: ", type(vm_nics))
+        pprint(vm_nics)
+
+        # Setting override_network_config to True here means it takes the network info we give it (vm_nics etc)
+        # instead of cloning it from the source VM.
+        vm_clone_proto = {
+            "override_network_config": True,
+            "name": vm_name,
+            "uuid": str(uuid.uuid4()),
+            "vm_nics": vm_nics,
+        }
+
+        print("VM_CLONE_PROTO: ", type(vm_clone_proto))
+        pprint(vm_clone_proto)
+
+        specs = []
+        specs.append(vm_clone_proto)
+        
+        vm_clone_spec = {}
+        vm_clone_spec["spec_list"] = specs
+        
+        cloneuuid =  vm_clone_spec['spec_list'][0]["uuid"]
+        print("Cloneuuid:",cloneuuid)
+        cluster_url = self.base_urlv2 + "vms/" + str(quote(vmid)) + "/clone"
+        print("Cloning VM",cluster_url)
+        try:
+            server_response = self.sessionv2.post(cluster_url, data=json.dumps(vm_clone_spec))
+            return server_response.status_code, cloneuuid, json.loads(server_response.text)
+        except Exception as ex:
+            print(ex)
+            return -1,0,cluster_url
+            
+    # Power on VM with this UUID.
+    def power_on_vm(self, vmid):
+
+        print("Powering on VM: %s." % vmid)
+        cluster_url = self.base_urlv2 + "vms/" + str(quote(vmid)) + "/set_power_state/"
+        vm_power_post = {"transition":"ON"}
+        server_response = self.sessionv2.post(cluster_url, data=json.dumps(vm_power_post))
+        print("Response code: %s" % server_response.status_code)
+        return server_response.status_code ,json.loads(server_response.text)
+
+    # Just hang out until the task is complete.
+    def poll_task(self, task_uuid):
+
+        cluster_url = self.base_urlv2 + "tasks/poll/"
+        vm_poll_post = {"completed_tasks": [task_uuid]}
+        print("Polling task %s. Cluster_URL %s" % (task_uuid,cluster_url))
+        # Loop until status is succeeded.
+        while True:
+            server_response = self.sessionv2.post(cluster_url, data=json.dumps(vm_poll_post))
+            r = json.loads(server_response.text)
+            ps = str(r["completed_tasks_info"][0]["progress_status"])
+            if ps == "Succeeded":
+                break
+                # End while loop
+        return
+
+    # Get network info. Called by get_usable_vlans() below.
+    def get_network_info(self):
+        
+        cluster_url = self.base_urlv2 + "/networks/"
+        print("Getting network info")
+        server_response = self.sessionv2.get(cluster_url)
+        print("Response code: %s" % server_response.status_code)
+        return server_response.status_code ,json.loads(server_response.text)
+        
+    # Return num usable VLANs.
+    def get_usable_vlans(self,num):
+
+        status,resp = self.get_network_info()
+        all_networks = resp["entities"]
+        
+        used_vlans = []
+        for network in all_networks:
+            used_vlans.append(network["vlan_id"])
+
+        # print("Used VLANS")
+        # pprint(used_vlans)
+
+        # Uncomment in production. All VLANs range from 0 to 4095.
+        # all_vlans = list(set(range(4096)))
+        all_vlans = [1102, 1103, 1104]
+        # print("All VLANS")
+        # pprint(all_vlans)
+
+        usable_vlans = list(set(all_vlans) - set(used_vlans))
+        usable_vlans.sort()
+        # print("Usable VLANs", len(usable_vlans))
+        # pprint(usable_vlans)
+        ret_vlans = usable_vlans[0:num]
+
+        return ret_vlans
+
+    # Create a network.
+    def create_network(self,vlan,prefix):
+
+        network_spec = {
+            "vlan_id": str(vlan),
+            "name": prefix + "_student." + str(vlan),
+            "annotation": "student." + str(vlan)
+        }
+
+        cluster_url = self.base_urlv2 + "networks/"
+        print("Creating network", cluster_url)
+        try:
+            server_response = self.sessionv2.post(cluster_url, data=json.dumps(network_spec))
+            return server_response.status_code,json.loads(server_response.text)
+        except Exception as ex:
+            print(ex)
+            return -1,cluster_url
+
+    # Update VM with network uuid.
+    def update_vm_network(self,vmid,network_uuid):
+        
+        vm_update_spec = {
+            "vm_nics": [
+                { "network_uuid": network_uuid }
+            ]
+        }
+
+        cluster_url = self.base_urlv2 + "vms/" + str(quote(vmid))
+        print("Updating VM",cluster_url)
+        try:
+            server_response = self.sessionv2.put(cluster_url, data=json.dumps(vm_update_spec))
+            return server_response.status_code, json.loads(server_response.text)
+        except Exception as ex:
+            print(ex)
+            return -1,cluster_url
